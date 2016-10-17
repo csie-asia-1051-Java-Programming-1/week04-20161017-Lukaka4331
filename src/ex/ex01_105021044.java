1a0bac5980c3d469412d3ca342efced106e6182c
Lukaka4331 <105021044@live.asia.edu.tw>
2016-10-18 00:36:53 +0800

    ex01

--- src/ex/ex01_105021044.java
+++ src/ex/ex01_105021044.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 /*
  * Topic: 寫一個程式讓使多個正整數(包含0)直到使用者輸入-1 時停止，同時要顯示這些數字 總合 及 平均。
  * Date: 2016/10/17
+ * Author: 105021044 鐘岑哲
  */
 
 public class ex01_105021044 {
@@ -20,6 +21,8 @@ public class ex01_105021044 {
 
         while(a!=-1){	
          a=scn.nextInt();
+        c++;sum+=a;	}	System.out.println("總和="+sum);		
+         c=c-1	;System.out.println("平均="+(float) sum/c);
         	
         
 
