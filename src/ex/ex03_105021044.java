d336916f30d41b9acf244a799b0f22b5f81f0740
Lukaka4331 <105021044@live.asia.edu.tw>
2016-12-14 12:42:11 +0800

    ex03

---
+++ src/ex/ex03_105021044.java
@@ -0,0 +1,35 @@
+﻿package ex;
+import java.util.Scanner;
+/*
+ * Topic: 讓使用者輸入 一個正整數 n 然後顯示出下列圖形 (例 n = 4)
+	* 
+	** 
+	***
+	****
+ * Date: 2016/10/17
+ * Author: 105021044 鐘岑哲
+ */
+
+
+public class ex03_105021044 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scn=new Scanner(System.in);
+		System.out.println("n=");
+	    int n=scn.nextInt();
+		int sum=1;
+		//說明有幾層
+		for(int i=0;i<n;i++){
+			for(int j=0;j<sum;j++){
+				System.out.print("*");//sum為一個疊加值
+			}		System.out.println();//為每層換行之功用
+			        sum++;//在此疊加循環
+			              //直到n結束為止 
+		}
+		
+		
+	}
+
+}
